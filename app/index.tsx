673bbe7b9417b8411c9a3a4afb47388e38a42ca8
lucas <lucasnumeriano.dev@gmail.com>
2025-10-02 09:03:53 -0300

    feat: add basic layout for home

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,42 @@
-import { Text, View } from 'react-native';
+import Button from '@/components/Button';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import React, { Fragment } from 'react';
+import { View } from 'react-native';
+import T from '../components/T';
+import Container from '@/components/Container';
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-slate-900">
-      <Text className="text-2xl font-bold text-white">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <Fragment>
+      <Container className="items-center bg-slate-900">
+        <T variant="h2" color="text-yellow-400">
+          Bem vindo ao
+        </T>
+        <T variant="h1" color="text-white">
+          EletroRef
+        </T>
+        <View className=" my-auto flex flex-row items-center gap-10">
+          <View className="flex flex-col items-center gap-5">
+            <Button
+              onPress={() => alert('Button Pressed')}
+              startIcon={<MaterialIcons name="bolt" size={48} color="#facc15" />}
+            />
+            <T variant="body" color="text-white">
+              Cálculos Elétricos
+            </T>
+          </View>
+          <View className="flex flex-col items-center gap-5">
+            <Button
+              onPress={() => alert('Button Pressed')}
+              startIcon={<FontAwesome6 name="book-open" size={48} color="#fff" />}
+            />
+            <T variant="body" color="text-white">
+              Tabelas
+            </T>
+          </View>
+        </View>
+      </Container>
+    </Fragment>
   );
 }
