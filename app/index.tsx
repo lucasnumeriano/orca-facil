56930553578eda52c2be61cacd79c601c4865a96
lucas <lucasnumeriano.dev@gmail.com>
2025-10-02 06:39:02 -0300

    chore: finish basic configs

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 items-center justify-center bg-slate-900">
+      <Text className="text-2xl font-bold text-white">Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
